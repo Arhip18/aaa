9f886ee6dec42d4da43fad6f707acebbdf4aea5d
Alexei <Alexeilebedev2006@gmail.com>
2024-03-26 16:48:19 +0400

    practic day 1

---
+++ windows/classes/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp52_практика.windows.classes
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ windows/classes/Class2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp52_практика.windows.classes
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLlocalDB;Initial Catalog=Mulgachev;Integrated Security=True;Connect Timeout=30;Encrypt=False;");
+        }
+    }
+}
